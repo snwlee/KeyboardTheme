987ce31aa3d0d606c95d924ceb163310ac396c34
swlee <swlee@netand.co.kr>
2025-10-16 14:09:07 +0900

    config: intl version up [1.0.0+1]

---
+++ keyboard_theme_app/android/app/src/main/kotlin/com/example/keyboard_theme_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.keyboard_theme_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ keyboard_theme_app/lib/l10n/app_localizations_ko.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Korean (`ko`).
+class AppLocalizationsKo extends AppLocalizations {
+  AppLocalizationsKo([String locale = 'ko']) : super(locale);
+
+  @override
+  String get appTitle => '키보드 테마';
+
+  @override
+  String get homeTitle => '키보드 테마 홈';
+
+  @override
+  String get homeMessage => '키보드 테마와 언어를 선택하세요.';
+}
